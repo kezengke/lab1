67b6aaab6906e27f4a63c32a71770430d57fc3c8
2kisa <2kisa@wifi_stu-10-38-56-121.uncc.edu>
2018-08-31 12:36:50 -0400

    added frequency calculation

--- src/lab1/lab1.java
+++ src/lab1/lab1.java
@@ -8,20 +8,33 @@ public class lab1
 	public static void main(String[] args)
 	{
 		Random random = new Random();
-		
+		System.out.println("(1)");
+		float count = 0;
 		for(int x=0; x<1000; x++)
 		{
 			int first = random.nextInt(4);
 			int second = random.nextInt(4);
 			int third = random.nextInt(4);
 			
-			
-			
+			if(first ==second && first == third)
+			{
+				++count;
+			}
+						
 			System.out.println(changing(first)+
 					changing(second)+changing(third));
-		}
 			
+		}
+		
+		System.out.println("Expected seeing 'AAA:'");
+		System.out.println(0.25*0.25*0.25);
+		System.out.println("Actually seeing 'AAA:'");
+		System.out.println(count/1000.0);
+		
+		
+		
 	}
+	
 	public static String changing(int a)
 	{
 		String b;
